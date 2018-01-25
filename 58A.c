a3863bb54fe8f66b37f72493e840685437ad5ebb
JeonMinkyu <33167720+JeonMinkyu@users.noreply.github.com>
2018-01-25 23:36:19 +0900

    Add files via upload

---
+++ 58A.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void){
+	char str[100]="hello";
+	char str2[100];
+	int i=0,j=0;
+
+	scanf("%s",str2);
+
+	for(i=0;i<strlen(str2);i++){
+		if(str2[i]==str[j]){
+			j++;
+		}
+	}
+	if(j==strlen(str))
+		printf("YES");
+	else
+		printf("NO");
+
+	return 0;
+}
+	
